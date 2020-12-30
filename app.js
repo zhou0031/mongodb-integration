4494be13b4411fdc06120f15a3de80cd925da728
Ryan Williams <ryanzwilliams@Ryans-MacBook-Pro.local>
2020-12-30 20:11:09 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+const express=require('express')
+const app=express()
+const indexRouter=require('./routes/index')
+
+//App
+app.set('view engine', 'pug')
+app.set('views',__dirname+'/views')
+app.use(express.static('public'))
+
+
+//MongoDB
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL,{useUnifiedTopology: true, useNewUrlParser: true})
+const db=mongoose.connection
+db.on('error',error=>console.error(error))
+db.once('open',()=>console.log('Connected to Mongoose'))
+
+//Routes
+app.use('/',indexRouter)
+
+app.listen(process.env.PORT||3000)
