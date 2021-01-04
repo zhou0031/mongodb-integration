169fc917c080b8331ca36d6e0555ca84f6339677
Ryan Williams <ryanzwilliams@Ryans-MacBook-Pro.local>
2021-01-04 09:28:33 +0800

    chnaged login page

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router  = express.Router()
 
-
 router.get('/',(req,res)=>{
-    res.render('admin/index',{"title":"Admin panel（管理员界面）"})
+    res.render('admin/login',{"title":"Admin panel（管理员界面）"})
 })
 
 module.exports = router
