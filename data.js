7b19a67a305cb8bec051c934f7a7fe5850b32391
Ryan Williams <ryanzwilliams@Ryans-MacBook-Pro.local>
2021-01-10 17:39:54 +0800

    data.js

--- data.js
+++ data.js
@@ -4,15 +4,5 @@ const ROLE = {
 }
 
 module.exports = {
-    ROLE: ROLE,
-    users: [
-      { id: 1, name: '周奕', username:"z_yi23", password:'$2a$10$jcpC82Dv6CJS72I4o2Ij7uu1BUKhswBcQ5R.rLWlQZJxK96JWOWlC', role: ROLE.ADMIN },
-      { id: 2, name: 'Sally', email:"1@1.com",password:"$2a$10$.XPTyeXIR5TM2XcTVHRaT.DqZnpmL7xKAUCKJhmdJMK/FTviOKH8.",role: ROLE.BASIC },
-      { id: 3, name: 'Joe', email:"2@2.com",password:"$2a$10$tJLuTJNr7YcPhQ8sBxTIUemq0sAJ6WXj7uRxqfA4HKXOBgy5RnNkC",role: ROLE.BASIC }
-    ],
-    carts: [
-      { id: 1, name: "周奕's cart", userID: 1 },
-      { id: 2, name: "Sally's cart", userID: 2 },
-      { id: 3, name: "Joe's cart", userID: 3 }
-    ]
+    ROLE: ROLE
   }
