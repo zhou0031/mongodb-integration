d259166109bec103eac1904dcee1b0f30328a34a
Ryan Williams <ryanzwilliams@Ryans-MacBook-Pro.local>
2021-02-23 20:46:44 +0800

    google token returned

--- public/js/google/google-auth.js
+++ public/js/google/google-auth.js
@@ -1,7 +1,21 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
+async function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token
+   
+    try{
+          await fetch("/user/google/verifyGoogleIdToken",{
+          method:"POST",
+          headers:{"Content-Type":"application/json"},
+          body:JSON.stringify({id_token})
+        })
+        .then(response => response.json())
+        .then(data=>{
+          
+        })
+    }catch(error){
+      console.log("Error: "+error)
+    }
     
-  }
+}
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
--- routes/user/google.js
+++ routes/user/google.js
@@ -1,22 +1,30 @@
 const express               = require('express')
 const router                = express.Router()
+const BasicUser             = require('../../models/basicUser')
 const {OAuth2Client}        = require('google-auth-library');
 const client                = new OAuth2Client(process.env.GOOGLE_AUTH_CLIENT_ID);
 
 
 router.post('/verifyGoogleIdToken', async(req,res)=>{
+    const token = req.body.id_token
+    
     try{
         const ticket = await client.verifyIdToken({
             idToken: token,
-            audience: process.env.GOOGLE_AUTH_CLIENT_ID,  
-            
+            audience: process.env.GOOGLE_AUTH_CLIENT_ID
         })
         const payload = ticket.getPayload()
-        const userid = payload['sub']
+        
+        if(payload['aud']==process.env.GOOGLE_AUTH_CLIENT_ID)
+            return res.json(payload)
+        else
+            res.status(401).send("the app is not intended for the user 客户端请求与Client ID 不符（GOOGLE_AUTH_CLIENT_ID）")
     }catch(error){
         console.log(error)
     }
 })
+
+
 
 
 //Modue export
