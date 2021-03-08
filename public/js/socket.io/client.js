83e2087ac7651d2cd1e5838569e29ed117ab3eca
zhou0031 <ryanzwilliams@Ryans-MacBook-Pro.local>
2021-03-08 10:18:57 +0800

    socketio

---
+++ public/js/socket.io/client.js
@@ -0,0 +1,3 @@
+const socket = io('http://localhost:3001',{
+  autoConnect:true
+})
---
+++ socket.io/server.js
@@ -0,0 +1,34 @@
+const httpServer = require("http").createServer()
+const io = require("socket.io")(httpServer,{
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+      }
+})
+/*********************************************************************** */
+io.on("connection", socket => { 
+    console.log("user connected")
+    
+    socket.on('chat-message', (message) => {
+        io.emit('chat-message', message)
+    })
+        
+   
+   
+
+
+
+
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+    socket.onAny((event, ...args) => {
+        console.log(event, args);
+    })
+ })
+/************************************************************************* */
+ httpServer.listen(3001,()=>{
+     console.log("chat server on 3001")
+ })
+
