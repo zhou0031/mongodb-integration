06acb3cad985bf9e1996d725fe48307cdecc8530
Ryan Williams <ryan.z.williams@me.com>
2021-03-10 12:43:46 +0800

    chat

--- public/js/socket.io/client.js
+++ public/js/socket.io/client.js
@@ -1,3 +1,24 @@
 const socket = io('http://localhost:3001',{
   autoConnect:true
 })
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+
+
+
+
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat-message', input.value);
+    input.value = ''
+  }
+})
+
+socket.on('chat-message', function(message) {
+  var item = document.createElement('li')
+  item.textContent = message
+  messages.appendChild(item)
+  window.scrollTo(0, document.body.scrollHeight)
+})
